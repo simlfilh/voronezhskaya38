f325fe4efd6656bb8b0b3ed780d7e208612980bd
Valeria <148010378+simlfilh@users.noreply.github.com>
2026-06-22 13:27:27 +0300

    Create 16_links.py

---
+++ voronezhskaya38/16_links.py
@@ -0,0 +1,76 @@
+import streamlit as st
+
+st.title("📲 Наши социальные сети")
+st.divider()
+
+st.markdown("""
+    <style>
+        .colored-container {
+           background-color: #FFFFFF; 
+           border-radius: 10px;     
+           padding: 10px;          
+           margin-bottom: 20px;     
+           color: black !important;
+           line-height: 1.0;
+           font-size: 21px;
+        }
+        .highlight-green {
+            background-color: #C8E6C9; 
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+            position: relative;
+        }
+        .highlight-red {
+            background-color: #FFCDD2; 
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+            position: relative;
+        }
+        .highlight-blue {
+            background-color: #B3E5FC;
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+            position: relative;
+        }
+        .text-indent-content {
+            position: relative;
+            left: 20px;
+            color: black;
+            line-height: 1.4;
+        }
+    </style>
+    <div class="colored-container">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+                <div style="display: flex; align-items: center; gap: 20px;">
+                    <a href="https://max.ru/id7840483155_gos17" target="_blank">
+                        <i class="fas fa-comment-dots fa-2x" style="color: #00BFFF;"></i>
+                    </a>
+                    <h3>Канал «Общежития СПбГЭУ» в MAX.</h3>
+                </div>
+        </div>
+            """, unsafe_allow_html=True)
+st.divider()
+
+st.markdown("**Контакты для связи:**")
+st.write("Заведующий общежитием: Малышева Елена Андреевна 👩🏼‍💼")
+st.markdown("""
+    <style>
+        .custom-links a {
+            color: white !important;
+            text-decoration: none; 
+        }
+        .custom-links a:hover {
+            color: #ccc !important;  
+            text-decoration: underline; 
+        }
+    </style>
+    <div class="custom-links">
+        <p>📞 <a href="tel:+78124589730,4961">(812) 458-97-30 (внутр. 4961)</a></p>
+    </div>
+""", unsafe_allow_html=True)
+st.divider()
+
+st.markdown("🆘 Свяжитесь со студенческим советом через соответствующий раздел")
